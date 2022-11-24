e2fe989e298245188b4d3a40908c45ab114b0423
Shamir Asharf <shamirkolakkadan26@gmail.com>
2022-11-24 02:49:25 +0000

    codespace update

---
+++ basic/type_conv.py
@@ -0,0 +1,7 @@
+# Type conversion
+# convert sprint to int,float,bool
+
+birth_year = input('Birth Year : ')
+age = 2022 - int(birth_year)
+
+print('Your age is : ' + str(age))
---
+++ basic/weight.py
@@ -0,0 +1,9 @@
+# convert lbs (pound) to kg
+
+#prompt for user 
+weight_kg = input('Your weight = ')
+
+#convert kg to lbs
+weight_lbs = int(weight_kg) * 2.20
+
+print('Your Weight is ' + str(weight_lbs))
